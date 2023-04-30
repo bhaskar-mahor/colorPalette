dce9cf1804d23958e5f32f3a2c891f46d7bff1d2
Bhaskar Mahor <126245280+developer-bhaskar@users.noreply.github.com>
2023-04-30 13:15:57 +0530

    color palette basic structure complete

---
+++ code.js
@@ -0,0 +1,51 @@
+primary_colors = [
+    ["#000000", "Black",[["#F7F7F7","Desert Storm"],["#E8E8E8","Mercury"],["#D9D9D9","Light Gray"],["#CACACA","Silver"],["#BBBBBB","Ash Gray"],["#ACACAC","Silver Charlice"],["#9D9D9D","Star Dust"],["#8E8E8E","Oslo Grey"],["#808080","Grey"],["#707070","Smokey Grey"],["#616161","Storm Dust"],["#525252","Vampire Grey"],["#434343","Black Eel"],["#343434","Dark Grey"],["#252525","Heavy Metal"],["#161616","Rangoon Green"]]],
+    ["#FFFFFF", "White",[["#fafafa","Alabaster"],["#F0F0F0","SeaShell"],["#E6E6E6","Mercury"],["#DBDBDB","Gainsboro"],["#D1D1D1","Grey Goose"],["#C7C7C7","Silver"],["#BDBDBD","Cotton Seed"],["#B3B3B3","Silk"],["#949494","Mountain Mist"],["#757575","Boulder"],["#6B6B6B","Dove Grey"],["#575757","Davy Grey"],["#4d4d4d","Liver"],["#424242","Charcoal Grey"],["#383838","Dark Grey"]]],
+    ["#FF0000", "Red",[["#FFEEEE","Pearl"],["#FFCCCC","Light Pink"],["#FFAAAA","Flamingo Pink"],["#FF8888","Rose Pink"],["#FF6666","Bittersweet"],["#FF4444","Light Red"],["#FF2222","Ruby Red"],["#DD0000","Cadmium Red"],["#BB0000","Scarlet"],["#990000","Crimson Red"],["#770000","Dark Red"],["#550000","Rustic Red"],["#330000","Chocklate"],["#110000","Diesel"]]],
+    ["#0000FF", "Blue",[["#eeeeff","Titan White"],["#ccccff","Periwinkle"],["#aaaaff","Caroline Blue"],["#8888ff","Lavender Blue"],["#6666ff","Light Slate Blue"],["#4444ff","Blue Orchid"],["#2222ff","Vivid Blue"],["#0000dd","Rich Blue"],["#0000bb","Medium Blue"],["#000099","Earth Blue"],["#000077","Navy Blue"],["#000055","Dark Blue"],["#000033","Midnight Blue"],["#000011","Jaguar"]]],
+    ["#00FFFF", "Cyan",[["#eeffff","Twilight Blue"],["#CCFFFF","Light Slate"],["#AAFFFF","Light Cyan"],["#88ffff","Light Cyan"],["#66FFFF","Blue Zircon"],["#44FFFF","Bright Cyan"],["#22FFFF","Bright Aqua"],["#00DDDD","Aqua Blue"],["#00BBBB","Topaz"],["#009999","Teal"],["#007777","Pine Green"],["#005555","Deep Teal"],["#003333","Sherwood Green"],["#001111","Blue Charcoal"]]],
+    ["#FF00FF", "Magenta",[["#FFEEFF","Lavender Blush"],["#FFCCFF","Very Light Purple"],["#FFAAFF","Blossom Pink"],["#FF88FF","Fuchsia Pink"],["#FF66FF","Ultra Pink"],["#FF44FF","Neon Pink"],["#FF22FF","Bright Magenta"],["#DD00DD","Hot Magenta"],["#BB00BB","Vibrant Purple"],["#990099","Barney Purple"],["#770077","Dark Magenta"],["#550055","Deep Violet"],["#330033","Mardi Gras"],["#110011","Jaguar"]]],
+    ["#FFFF00", "Yellow",[["#ffffee","Ivory"],["#FFFFCC","Moon Glow"],["#FFFFAA","Parchment"],["#FFFF88","Butter"],["#FFFF66","Custard"],["#FFFF44","Canary Yellow"],["#FFFF22","Sunny Yellow"],["#DDDD00","Bitter Lemon"],["#BBBB00","Olive Yellow"],["#999900","Brown Yellow"],["#777700","Olive"],["#555500","Green Brown"],["#333300","Dark Olive"],["#111100","Black Forest"]]],
+    ["#8F00FF", "Purple",[["#F8EFFF","Pearl"],["#EACFFF","Pearl Lavender"],["#DCAFFF","Mauve"],["#CE8FFF","Baby Purple"],["#C070FF","Easter Purple"],["#B250FF","Jasmine Purple"],["#A430FF","Electric Purple"],["#9610FF","Vivid Violet"],["#7400CF","Dark Violet"],["#6200AF","Violet Blue"],["#50008F","Indigo"],["#3F0070","Royal Purple"],["#2D0050","Cherry Pie"],["#1B0030","Black Rock"]]],
+    ["#FF6600", "Orange",[["#FFF7F2","Rose White"],["#FFE8D9","Misty Rose"],["#FFD9BF","Peach Puff"],["#FFC9A6","Deep Peach"],["#FFBA8C","Pale Salmon"],["#FFAB73","Apricot"],["#FF9C59","Atomic Tangerine"],["#FF8C40","Mango Orange"],["#FF7D26","Cadmium Orange"],["#FF6E0D","Pumpkin Orange"],["#F26100","Bright Orange"],["#D95700","Deep Orange"],["#BF4D00","Burnt Orange"],["#A64200","Fire"],["#8C3800","Rust Brown"],["#732E00","Chestnut"],["#592400","Brown Bramble"],["#401A00","Chocolate Brown"],["#260F00","Wood Bark"]]],
+    ["#00FF00", "Green",[["#F2FFF2","Honeydew"],["#D9FFD9","Snowy Mint"],["#BFFFBF","Light Jade"],["#A6FFA6","Pastel Green"],["#8CFF8C","Easter Green"],["#73FF73","Screaming Green"],["#59FF59","Bright Green"],["#40FF40","Light Green"],["#26FF26","Hot Green"],["#0DFF0D","Highlight Green"],["#00D900","Vibrant Green"],["#00BF00","Kelly Green"],["#00A600","Irish Green"],["#008C00","Tree Green"],["#007300","Japanese Laurel"],["#005900","Deep Green"],["#004000","Hunter Green"],["#002600","Dark Green"]]],
+    ["#FF1493", "Deep Pink",[["#FFF3F9","Wisp Pink"],["#FFDBEE","Pink Lace"],["#FFC0CB","Pink"],["#FFC2E3","Classic Rose"],["#FFAAD8","Powder Pink"],["#FF92CD","Pastel Magenta"],["#FF79C2","Persian Pink"],["#FF61B6","Hot Pink"],["#FF49AB","Wild Strawberry"],["#FF31A0","Persian Rose"],["#E7007D","Dark Pink"],["#CE0070","Red Violet"],["#B60062","Flirt"],["#9E0055","Dark Fuchsia"],["#860048","Red Purple"],["#6D003B","Black Rose"],["#55002E","Blackberry"],["#3D0021","Barossa"],["#240014","Sepia Black"]]],
+    ["#8B4513", "Brown",[["#FEF8F4","Vista White"],["#FBEADF","Antique White"],["#F8DDCA","Almond"],["#F5CFB4","Deep Peach"],["#F2C19F","Wax Flower"],["#EFB489","Tacao"],["#ECA674","Brown Sugar"],["#E9985F","Faded Orange"],["#E68B49","Dusty Orange"],["#E37D34","Dull Orange"],["#E06F1F","Papaya Orange"],["#CB651C","Cocoa Brown"],["#B65A19","Light Brown"],["#A05016","Hawaiian Tan"],["#8B4513","Bull Shot"],["#763A10","Copper Canyon"],["#60300D","Saddle Brown"],["#4B250A","Brown Derby"],["#47230a","Deep Oak"],["#331907","Dark Brown"],["#1F0F04","Creole"],["#0A0501","Night"]]],
+    ["#003399", "Royal Azure",[["#F3F7FF","Alice Blue"],["#DCE8FF","Lavender Mist"],["#C5D8FF","Tropical Blue"],["#AEC9FF","Baby Blue"],["#97B9FF","Jordy Blue"],["#80AAFF","Denim Blue"],["#689BFF","Cornflower Blue"],["#518BFF","Dodger Blue"],["#3A7CFF","Clear Blue"],["#236CFF","Azul"],["#0C5DFF","Bright Blue"],["#0051F3","Ocean Blue"],["#0049DC","Blueberry"],["#003AAE","Royal Azure"],["#002B80","Cataline Blue"],["#002368","Marine"],["#001B51","Gulf Blue"],["#000C23","Black Pearl"]]],
+    ["#6B8E23", "Olive Drab",[["#9ACD32","Yellow Green"],["#556B2F","Dark Olive Green"]]],
+    ["#BA55D3", "Medium Orchid",[["#DA70D6","Orchid"],["#E0B0FF","Mauve"]]],
+    ["#808000", "Olive",[["#F0E68C","Khaki"],["#BDB76B","Dark Khaki"]]],
+    ["#32CD32", "Lime",[["#00FF00","Lime Green"],["#7FFF00","Chartreuse"],["#7CFC00","Lawn Green"]]],
+    ["#800000", "Maroon",[["#8B0000","Dark Red"],["#A52A2A","Hot Red"],["#B22222","Fire Brick"],["#DC143C","Crimson"]]],
+    ["#F5DEB3", "Wheat",[["#FFE4C4","Bisque"],["#FFEBCD","Balanched Almond"],["#FFFDD0","Cream"],["#FFF8DC","Corn Silk"],["#FFFACD","Lemon Chiffon"]]],
+]
+var color_name;
+var color_code;
+
+window.addEventListener("load",function(event){
+    let html = '';
+    for(let i=0;i<primary_colors.length;i++){
+        html+=`<div class="select_color_box_list primary_color" onclick="shades(this)" name="${primary_colors[i][1]}" id="${primary_colors[i][0].substring(1)}" code="${primary_colors[i][0]}" style="background: ${primary_colors[i][0]};"></div>`
+    }
+    this.document.getElementsByClassName("primary_section")[0].innerHTML = html;
+})
+
+function shades(event){
+    change_background(event);
+    let color_name = event.getAttribute("name");
+    let color_code = event.getAttribute("code");
+    let html = '';
+    for(let i=0;i<primary_colors.length;i++){
+        if(primary_colors[i][0]==color_code){
+            for(let j=0;j<primary_colors[i][2].length;j++){
+                html += `<div class="select_color_box_list secondary_color" onclick="change_background(this)" name="${primary_colors[i][2][j][1]}" code="${primary_colors[i][2][j][0]}" id="${primary_colors[i][2][j][0].substring(1)}" style="background: ${primary_colors[i][2][j][0]};"></div>`
+            }
+        }
+    }
+    this.document.getElementsByClassName("secondary_section")[0].innerHTML = `<p>Shades of ${color_name}</p>${html}`;
+}
+
+function change_background(event){
+    let code = event.getAttribute("code");
+    document.getElementById("body").style.backgroundColor = code;
+}
